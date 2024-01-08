6d96191dd1d19b1fdbde8eaa742dda6646e5b139
icekimchi <pjh010828@gmail.com>
2024-01-08 10:31:21 +0900

    디렉터리 수정

--- 04/04.java
+++ 04/04.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DowhileExmaple{
     public static void main(String[] args){
-        System.out.prinln("메시지를 입력하세요");
+        System.out.println("메시지를 입력하세요");
         System.out.println("프로그램을 종료하려면 q를 입력하세요");
 
         //Scanner 객체 생성
@@ -16,6 +16,6 @@ public class DowhileExmaple{
             System.out.println(inputString);
         } while (! inputString.equals("q")); //문자열을 비교할 때는 equals() 메소드 사용
 
-        System.out.println(프로그램 종료);
+        System.out.println("프로그램 종료");
     }
 }
