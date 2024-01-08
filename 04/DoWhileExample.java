99711258fecdca28404c5a08d05424b7a19dc28a
icekimchi <pjh010828@gmail.com>
2024-01-08 15:54:26 +0900

    0108 2차원 배열 ~ 클래스 공부

--- 04/DoWhileExample.java
+++ 04/DoWhileExample.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DowhileExmaple{
+public class DoWhileExample{
     public static void main(String[] args){
         System.out.println("메시지를 입력하세요");
         System.out.println("프로그램을 종료하려면 q를 입력하세요");
