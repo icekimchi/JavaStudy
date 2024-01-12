3c785c10141c6d941f917e070081ec5458b706b0
icekimchi <pjh010828@gmail.com>
2024-01-12 17:04:25 +0900

    7장 상속 - final

---
+++ ch07/CellPhone.java
@@ -0,0 +1,15 @@
+package ch07;
+
+
+public class CellPhone{
+    //필드
+    String model;
+    String color;
+
+    //생성자
+    
+    //메소드
+    void poweron() {System.out.println("전원을 켭니다."); }
+    void powerOff() {System.out.println("전원을 끕니다.");}
+    void bell() {System.out.println("벨이 울립니다.");}
+}
